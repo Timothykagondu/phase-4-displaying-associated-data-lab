5ad44a9e926c04a465d5cd92dc4ff5481c4375e4
TimothyKagondu <timothy.2k21@gmail.com>
2023-03-21 15:44:43 +0300

    completed assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+
+    def show
+        users = User.all
+        render json: users, except: [:created_at, :updated_at], include: {items: {except: [:user_id, :created_at, :updated_at]}}
+      end
+      
 end
